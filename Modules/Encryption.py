6ac70d8338a7850a3c9f22a8e57ee19015e2cf3d
Adar <warmundadar@gmail.com>
2020-08-16 21:51:28 +0300

    .

--- Modules/Encryption.py
+++ Modules/Encryption.py
@@ -5,9 +5,7 @@ import os
 
 class DatabaseEncrypt(object):
     def __init__(self):
-        try:
-            keyring.get_password('fbdb', os.environ.get('USER'))
-        except FileNotFoundError:
+        if keyring.get_password('fbdb', os.environ.get('USER')) is None:
             keyring.set_password('fbdb', os.environ.get('USER'), Fernet.generate_key())
         self.f = Fernet(keyring.get_password('fbdb', os.environ.get('USER')))
 
